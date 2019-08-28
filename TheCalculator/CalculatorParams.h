e1dcdf51d838d53c895de0e1ff0679320af6ec49
Shaun Mitchell <35670430+ShaunMitchell98@users.noreply.github.com>
2019-08-28 22:26:12 +0100

    Add files via upload

--- TheCalculator/CalculatorParams.h
+++ TheCalculator/CalculatorParams.h
@@ -9,7 +9,6 @@ enum AngleUnit {
 struct CalculatorParams {
 	double CurrentNumber = 0.0;
 	bool Decimal = false;
-	bool Exp = false;
 	double Multiplier = 1.0;
 	Platform::String^ DisplayOutput = "0";
 	std::vector<Platform::String^> Tokens;
--- TheCalculator/RunCalculations.h
+++ TheCalculator/RunCalculations.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 #include "CalculatorParams.h"
-#include "CharacterParser.h"
+#include "TokenParser.h"
 #include "Calculator.h"
 #include "CloseBrackets.h"
 #include <vector>
@@ -9,55 +9,66 @@
 
 double RunCalculations(CalculatorParams params)
 {
-	CharacterParser parser;
+	TokenParser parser;
 	Calculator calculator;
+	
+	//This will store the result of the calculations.
 	double result = 0.0;
+
+	//If no operators followed the last number typed, it will still be stored
+	//in CurrentNumber, it needs to be pushed to Tokens.
 	if (params.CurrentNumber != 0) {
 		params.Tokens.push_back(params.CurrentNumber.ToString());
-		params = parser.TerminateNumber(params);
+		params = parser.ResetNumberParams(params);
 	}
 
-
+	//If there is only a single token, we assume it is a number and return
+	//it as the result.
 	if (params.Tokens.size() == 1) {
 		result = _wtof(params.Tokens[0]->Data());
 	}
 
-	//Check for any unclosed brackets. If any are found the last open bracket
-	//is closed at the end. All others are closed two tokens away, such that
-	//there is one token between the open and closed bracket.
+	//Check for any unclosed brackets. If any are found, closed brackets
+	//are pushed to the end of Tokens until all open brackets are closed.
 	params.Tokens = CloseBrackets(params.Tokens);
 	
-	//Loop over each open bracket and pass the numbers between this
-	//open bracket and the close bracket with the same index to 
-	//HandleBracketedExpression.
-	
+	//Loop over each token and if it is an open bracket, pass the tokens between this
+	//open bracket and the corresponnding closed bracket to 
+	//EvaluateBracketedExpression. These tokens are removed from the Tokens vector
+	//and replaced with the outut of the function.
 	for (int i = 0; i < params.Tokens.size(); i++) {
 		
-		//Check each token to see if it is an open bracket.
 		if (params.Tokens[i] == "(") {
 			int StartIndex = i;
 			int EndIndex = calculator.FindEndIndex(params.Tokens, StartIndex);
 			std::vector<Platform::String^> BracketedExpression(params.Tokens.begin() + 
 				StartIndex, params.Tokens.begin() + EndIndex + 1);
-				
+			
+			//Remove bracketed expression from Tokens.
 			for (int i = EndIndex; i >= StartIndex; i--) {
 				params.Tokens.erase(params.Tokens.begin() + i);
 			}
+
+			//Place result of bracketed expression where the bracketed expression
+			//used to be.
 			params.Tokens.emplace(params.Tokens.begin() + StartIndex, 
 				calculator.EvaluateBracketedExpression(BracketedExpression, params.Unit));
 			i = 0;
 			}
 	}
 
+	//Check Tokens for Unary operations and perform them.
 	for (int i = 0;  i < params.Tokens.size(); i++) {
 		if (parser.IsUnaryOperator(params.Tokens[i])) {
+			//For these four unary operations, the operators follow their operands, for all
+			//other operations, the operator precedes the operand.
 			if (params.Tokens[i] == L"x²" || params.Tokens[i] == L"x³" || 
 				params.Tokens[i] == L"xʸ" || params.Tokens[i] == L"n!") {
 				params.Tokens[i-1] = calculator.EvaluateUnaryOperation(params.Tokens[i], params.Tokens[i-1], params.Unit);
 				params.Tokens.erase(params.Tokens.begin() + i);
 				//This line restarts the loop from the beginning of tokens. This
 		        //may be needed if index changes executed by 
-		        //HandleUnaryOperation mess up the indexing here.
+		        //EvaluateUnaryOperation mess up the indexing here.
 				i = 0;
 		   }
 			else {
@@ -68,6 +79,9 @@ double RunCalculations(CalculatorParams params)
 			}
 		}
 	}
+
+	//These for loops calculate binary operations. Multiplication and division are performed
+	//before addition and subtraction, as per BIDMAS.
 	for (int i = 0; i < params.Tokens.size(); i++) {
 		if (params.Tokens[i] == "X" || params.Tokens[i] == "÷") {
 			params.Tokens[i - 1] = calculator.EvaluateBinaryOperation(params.Tokens[i - 1],
